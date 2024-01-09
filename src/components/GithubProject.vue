14cc164f491ed14e877cf894274114cbc16c3267
张世昌 <15832065429@163.com>
2024-01-09 17:04:11 +0800

    初次提交

---
+++ src/components/GithubProject.vue
@@ -0,0 +1,152 @@
+<template>
+  <div class="github-project">
+    <!-- <div class="title">
+      <github-one theme="two-tone" size="24" :fill="['#efefef', '#00000020']" />
+      <span>Github</span>
+    </div> -->
+    <Swiper :modules="[Pagination, Mousewheel]" :slides-per-view="1" :space-between="40" :pagination="{
+      el: '.swiper-pagination',
+      clickable: true,
+      bulletElement: 'div',
+    }" :mousewheel="true">
+      <SwiperSlide v-for="list in projectList" :key="list">
+        <el-row class="all-project" :gutter="20">
+          <el-col v-for="(item, index) in list" :span="12" :key="index">
+            <div class="project cards" @click="toGithub(item)">
+              <div class="name">
+                <bookmark theme="outline" size="22" fill="#efefef" />
+                <div class="name-text">
+                  <span class="author">{{ item.author }}</span>
+                  <span>{{ item.name }}</span>
+                </div>
+              </div>
+              <span class="desc">{{ item.desc }}</span>
+            </div>
+          </el-col>
+        </el-row>
+      </SwiperSlide>
+      <div class="swiper-pagination" />
+    </Swiper>
+  </div>
+</template>
+
+<script setup>
+import { Bookmark } from "@icon-park/vue-next";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Pagination, Mousewheel } from "swiper";
+
+// 仓库数据
+const projectData = [
+
+];
+
+// 计算网站链接
+const projectList = computed(() => {
+  const result = [];
+  for (let i = 0; i < projectData.length; i += 4) {
+    const subArr = projectData.slice(i, i + 4);
+    result.push(subArr);
+  }
+  return result;
+});
+
+// 跳转至 Github
+const toGithub = (data) => {
+  window.open(`https://github.com/${data.author}/${data.name}`);
+};
+</script>
+
+<style lang="scss" scoped>
+.github-project {
+  width: 100%;
+  margin-top: 20px;
+
+  .title {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin: 0.2rem 0 1.5rem;
+    font-size: 1.1rem;
+
+    .i-icon {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-right: 6px;
+    }
+  }
+
+  .swiper {
+    left: -10px;
+    width: calc(100% + 20px);
+    padding: 5px 10px 0;
+    z-index: 0;
+
+    .swiper-slide {
+      height: 100%;
+    }
+
+    .swiper-pagination {
+      position: static;
+      margin-top: -8px;
+
+      :deep(.swiper-pagination-bullet) {
+        background-color: #fff;
+        width: 18px;
+        height: 4px;
+        border-radius: 4px;
+        transition: opacity 0.3s;
+
+        &:hover {
+          opacity: 1;
+        }
+      }
+    }
+  }
+
+  .all-project {
+    width: calc(100% + 20px);
+
+    .project {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      margin-bottom: 20px;
+      padding: 12px;
+      height: 100px;
+      background-color: transparent;
+
+      .name {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        margin-bottom: 8px;
+
+        .i-icon {
+          display: flex;
+          margin-right: 6px;
+        }
+
+        .author {
+          opacity: 0.8;
+
+          &::after {
+            content: "/";
+            margin: 0 4px;
+          }
+        }
+      }
+
+      .desc {
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: 2;
+        overflow: hidden;
+        word-break: break-all;
+        font-size: 13px;
+        opacity: 0.8;
+        line-height: 20px;
+      }
+    }
+  }
+}</style>
---
+++ src/views/MoreSet/index.vue
@@ -0,0 +1,169 @@
+<template>
+  <div class="set" @mouseenter="closeShow = true" @mouseleave="closeShow = false" @click.stop>
+    <transition name="el-fade-in-linear">
+      <close-one
+        class="close"
+        theme="filled"
+        size="28"
+        fill="#ffffff60"
+        v-show="closeShow"
+        @click="store.setOpenState = false"
+      />
+    </transition>
+    <el-row :gutter="40">
+      <el-col  class="right">
+        <div class="title">
+          <setting-two theme="filled" size="28" fill="#ffffff60" />
+          <span class="name">全局设置</span>
+        </div>
+        <Set />
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script setup>
+import { CloseOne, SettingTwo} from "@icon-park/vue-next";
+import { mainStore } from "@/store";
+import Set from "@/components/Set.vue";
+
+const store = mainStore();
+const closeShow = ref(false);
+
+</script>
+
+<style lang="scss" scoped>
+.set {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -webkit-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  width: 80%;
+  height: 80%;
+  background: rgb(255 255 255 / 40%);
+  border-radius: 6px;
+  padding: 40px;
+
+  .close {
+    position: absolute;
+    top: 14px;
+    right: 14px;
+    width: 28px;
+    height: 28px;
+
+    &:hover {
+      transform: scale(1.2);
+    }
+
+    &:active {
+      transform: scale(1);
+    }
+  }
+
+  .el-row {
+    height: 100%;
+    flex-wrap: nowrap;
+
+    .left {
+      height: 100%;
+      padding-left: 40px !important;
+      padding-bottom: 20px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+
+      .logo {
+        transform: translateY(-8%);
+        font-family: "Pacifico-Regular";
+        padding-left: 22px;
+        width: 100%;
+        height: 260px;
+        min-height: 140px;
+        .bg {
+          font-size: 5rem;
+        }
+
+        .sm {
+          margin-left: 6px;
+          font-size: 2rem;
+        }
+      }
+
+      .version {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+
+        .num {
+          font-size: 2rem;
+          font-family: "Pacifico-Regular";
+        }
+
+        .github {
+          width: 24px;
+          height: 24px;
+          margin-left: 12px;
+          margin-top: 6px;
+
+          &:hover {
+            transform: scale(1.2);
+          }
+        }
+      }
+
+      .update {
+        margin-top: 30px;
+        height: 100%;
+
+        :deep(.el-card__body) {
+          height: 100%;
+
+          .upnote {
+            padding: 20px;
+            height: calc(100% - 56px);
+            overflow-y: auto;
+
+            .uptext {
+              display: flex;
+              flex-direction: row;
+              align-items: center;
+              padding-bottom: 16px;
+
+              &:nth-last-of-type(1) {
+                padding: 0;
+              }
+
+              .i-icon {
+                width: 22px;
+                height: 22px;
+                margin-right: 8px;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    .right {
+      height: 100%;
+      display: flex;
+      flex-direction: column;
+
+      .title {
+        display: flex;
+        align-items: center;
+        flex-direction: row;
+        font-size: 18px;
+        margin-bottom: 16px;
+
+        .i-icon {
+          width: 28px;
+          height: 28px;
+          margin-right: 6px;
+        }
+      }
+    }
+  }
+}
+</style>
